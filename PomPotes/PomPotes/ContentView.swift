5be2514ff49073c7b493752afdccd202425502e7
devsantostiago <dev.santostiago@gmail.com>
2021-10-01 21:47:49 +0200

    Created empty SwiftUI project

---
+++ PomPotes/PomPotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PomPotes
+//
+//  Created by Tiago Santos on 01/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PomPotes/PomPotes/PomPotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomPotesApp.swift
+//  PomPotes
+//
+//  Created by Tiago Santos on 01/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PomPotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
